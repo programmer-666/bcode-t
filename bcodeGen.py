b6b3b4aa83f1866cd4a4565a31e134f5d9ff651d
Süha <suharslan@protonmail.ch>
2023-09-22 17:10:12 +0300

    Add files via upload

---
+++ bcodeGen.py
@@ -0,0 +1,14 @@
+from pandas import read_csv
+from barcode import Code128
+from barcode.writer import ImageWriter
+
+props = {
+    "module_width": 0.5,
+    "module_height": 4.5,
+    "font_size": 12,
+    "text_distance": 5,
+    "quiet_zone": 2,
+}
+
+for code in read_csv("wMachineCodes.csv")["kodlar"]:
+    Code128(str(code), writer=ImageWriter()).save("barcodes/" + str(code), props)
